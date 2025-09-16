ac8deb98647ec48723414294cce38f1e06030106
SDE1Junaid <jk1240651@gmail.com>
2025-09-16 22:24:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(cors({
-  origin: ['https://qurinom-solutions-assignment.vercel.app'],
+  origin: ['https://qurinom-solutions-assignment.vercel.app','http://localhost:3000'],
   credentials: true,
 }));
 app.use(cookieParser()); // Parse cookies
